46e96a37bc638c08bffd9c365a766b371958516a
Casey Baker <cj-baker@live.com>
2025-04-07 16:50:34 -0700

    Added articles into the database and displayed in the view

--- app/Controllers/Articles.php
+++ app/Controllers/Articles.php
@@ -2,11 +2,19 @@
 
 namespace App\Controllers;
 
+use App\Models\ArticleModel;
+
 class Articles extends BaseController 
 {
     public function index()
     {
+
+        $model = new ArticleModel;
         
-        return view ("Articles/index");
+        $data = $model->findAll();
+        
+        return view ("Articles/index", [
+            "articles" => $data
+        ]);
     }
 }
