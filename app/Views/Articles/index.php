389d46b6464ec9e988d513b70c24b1a65804333c
Casey Baker <cj-baker@live.com>
2025-04-07 16:58:04 -0700

    Updated view to show data properly

--- app/Views/Articles/index.php
+++ app/Views/Articles/index.php
@@ -2,6 +2,15 @@
 <?= $this->section("title") ?>Article<?= $this->endSection() ?>
 
 <?= $this->section("content") ?>
-<h1>Articles</h1>
+
+    <h1>Articles</h1>
+
+    <?php foreach ($articles as $article): ?>
+        <article>
+            <h2><?= $article["title"] ?></h2>
+            <p><?= $article["content"] ?></p>
+        </article>
+
+    <?php endforeach; ?>
 
 <?= $this->endSection() ?>
