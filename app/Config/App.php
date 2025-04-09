d03c4fb9d4b3aad565158b32714ed2ca8379563e
Casey Baker <cj-baker@live.com>
2025-04-08 23:19:13 -0700

    added css file, bootsrap and jquery

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://https://aqua-wren-447083.hostingersite.com/';
+    public string $baseURL = 'http://aqua-wren-447083.hostingersite.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
--- app/Views/Articles/new.php
+++ app/Views/Articles/new.php
@@ -4,17 +4,17 @@
 
 <?= $this->section("content") ?>  
 
-<h1>New Article</h1>
+<h1 class="mx-auto text-center mb-5 py-5">New Blog Entry</h1>
 
 <?= form_open("articles/create") //form needs to be added to the "helper" portion of the BaseController for this to work ?>
-
-<label for="title">Title</label>
-<input type="text" id="title" name="title" class="d-block">
+<div class="d-flex flex-column align-items-start mx-auto">
+<label for="title" class="">Title</label>
+<input type="text" id="title" name="title" class="form-control">
 
 <label for="content">Content</label>
-<textarea name="content" id="content" class="d-block"></textarea>
+<textarea name="content" id="content" class="d-block form-control"></textarea>
 
-<button class="btn btn-primary">Save</button>
-
+<button class="btn btn-primary mt-5 mx-auto px-5">Save</button>
+</div>
 </form>
 <?= $this->endSection() ?>
