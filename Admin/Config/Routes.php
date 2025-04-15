c260bc42c1225b688e6092582c22564e9cd2263e
Casey Baker <cj-baker@live.com>
2025-04-14 21:56:04 -0700

    Created a login filter, which filters access to certain routes based on whether or not the user is logged in

--- Admin/Config/Routes.php
+++ Admin/Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 //using the group method allows you to group routes that are using the same root and controller namespace.
-$routes->group("admin", ["namespace" => "Admin\Controllers"], static function ($routes){ 
+$routes->group("admin", ["namespace" => "Admin\Controllers", "filter" => "login"], static function ($routes){ 
     $routes->get("users", "Users::index");
     $routes->get("users/(:num)", "Users::show/$1");
     $routes->post("users/(:num)/toggle-ban", "Users::toggleBan/$1");
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,15 +8,16 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 service('auth')->routes($routes); //shield authorization routes
-$routes->resource("articles", ["placeholder" => "(:num)"]); // the resource route automatically generates all of the RESTful crud routes below
-//$routes->get("/articles", "Articles::index");
-//$routes->get("articles/(:num)", "Articles::show/$1");
-//$routes->get("articles/new", "Articles::new", ["as" => "new_article"]);
-//$routes->post("articles", "Articles::create");
-//$routes->get("articles/(:num)/edit", "Articles::edit/$1");
-//$routes->patch("articles/(:num)", "Articles::update/$1"); 
-//$routes->delete("articles/(:num)", "Articles::delete/$1");
-$routes->get("articles/(:num)/delete", "Articles::confirmDelete/$1");
-
-$routes->get("set-password", "Password::set");
-$routes->post("set-password", "Password::update");
+//$routes->resource("articles", ["placeholder" => "(:num)"]); // the resource route automatically generates all of the RESTful crud routes below
+$routes->get("/articles", "Articles::index");
+$routes->get("articles/(:num)", "Articles::show/$1");
+$routes->group("", ["filter" => "login"], static function ($routes){
+    $routes->get("articles/new", "Articles::new");
+    $routes->post("articles", "Articles::create");
+    $routes->get("articles/(:num)/edit", "Articles::edit/$1");
+    $routes->patch("articles/(:num)", "Articles::update/$1"); 
+    $routes->delete("articles/(:num)", "Articles::delete/$1");
+    $routes->get("articles/(:num)/delete", "Articles::confirmDelete/$1");
+    $routes->get("set-password", "Password::set");
+    $routes->post("set-password", "Password::update");
+});
