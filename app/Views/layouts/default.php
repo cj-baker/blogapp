1fb484aab2daae0ad7343b998669faf036ce48fd
Casey Baker <cj-baker@live.com>
2025-04-15 21:19:24 -0700

    installed tinymce textarea editor

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $this->renderSection("title") ?></title>
+    <script src="<?= base_url('assets/tinymce/tinymce.min.js'); ?>" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css'); ?>">
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,3 +2,9 @@ let table = new DataTable('#myTable', {
   paging: true,
   
 });
+
+tinymce.init({
+  selector: '#mytextarea',
+  plugins: 'advlist link image lists',
+  license_key: 'gpl'
+});
